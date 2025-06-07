527cddd39298b628d99890f5eac13dc8885c9abe
Right-side123 <rohit.yadav@rightside.co.in>
2025-06-07 12:51:41 +0530

    add cross button in header moddel

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import rightsideLogo from './Assets/rightsidelogo.svg';
 import dashboardicon from './Assets/dashbaordheadericon.png';
@@ -16,9 +16,21 @@ const HeaderCompo = () => {
 
   const [supportModel, setSupportModel] = useState(false)
 
+  const location = useLocation();
+
+  useEffect(() => {
+    setSupportModel(false);
+  }, [location]);
+
+
+  // const handleSupportClick = () => {
+  //   setSupportModel(true);
+  // };
+
   const handleSupportClick = () => {
-    setSupportModel(true);
+    setSupportModel(prev => !prev);
   };
+
 
 
   return (
@@ -51,8 +63,7 @@ const HeaderCompo = () => {
               {/* <span> */}
               <button className="support_button"
                 onClick={handleSupportClick}
-                onMouseOver={() => setSupportModel(true)}
-                onMouseOut={() => setSupportModel(false)}>
+              >
                 Support
               </button>
               {/* </span> */}
@@ -63,39 +74,49 @@ const HeaderCompo = () => {
         </div>
       </header>
       {supportModel && (
-        <div className='support_page'>
-          <div className='Support_Container'>
-            <div className='contact_details_container'>
-              <h1 className='contactdetails_heading'>Contact Details</h1>
-              <div className='location_details'>
-                <img src={locationicon} alt='address' className='location_icon' />
-                <span className='contact_details'>10th Floor, Aston tower, Sundervan Complex, <br />
-                  Shashtri Nagar, Andheri West, Mumbai, 4000053, <br />
-                  Maharashtra, India.</span><br />
-              </div>
-              <div>
-                <img src={mailicon} alt='e-mail' className='mail_icon' />
-                <span className="contact_email">
-                  <NavLink to="mailto:helpdesk@rightside.co.in" className="contact_email">helpdesk@rightside.co.in</NavLink>
-                </span><br />
-              </div>
-              <div className='phone_container'>
-                <img src={whatsappicon} alt='call' className='call_icon' />
-                <span className='contact_phone'>
-                  <NavLink to="tel:9987324949" className="contact_phone">+91 99873 24949</NavLink>
-                </span>
-              </div>
-              <div className='phone_container'>
-                <img src={callicon} alt='landline' className='call_icon' />
-                <span className='contact_phone'>
-                  <NavLink to="tel:2268996899" className="contact_phone"> +91 22 6899 6899</NavLink>
-                </span>
+        <div className='support_overlay'
+          onClick={() => setSupportModel(false)}>
+          <div className='support_page' onClick={(e) => e.stopPropagation()}>
+            <div className='Support_Container' >
+              <button
+                className="close_btn"
+                onClick={() => setSupportModel(false)}
+              >
+                ✖
+              </button>
+              <div className='contact_details_container'>
+                <h1 className='contactdetails_heading'>Contact Details</h1>
+                <div className='location_details'>
+                  <img src={locationicon} alt='address' className='location_icon' />
+                  <span className='contact_details'>10th Floor, Aston tower, Sundervan Complex, <br />
+                    Shashtri Nagar, Andheri West, Mumbai, 4000053, <br />
+                    Maharashtra, India.</span><br />
+                </div>
+                <div>
+                  <img src={mailicon} alt='e-mail' className='mail_icon' />
+                  <span className="contact_email">
+                    <NavLink to="mailto:helpdesk@rightside.co.in" className="contact_email">helpdesk@rightside.co.in</NavLink>
+                  </span><br />
+                </div>
+                <div className='phone_container'>
+                  <img src={whatsappicon} alt='call' className='call_icon' />
+                  <span className='contact_phone'>
+                    <NavLink to="tel:9987324949" className="contact_phone">+91 99873 24949</NavLink>
+                  </span>
+                </div>
+                <div className='phone_container'>
+                  <img src={callicon} alt='landline' className='call_icon' />
+                  <span className='contact_phone'>
+                    <NavLink to="tel:2268996899" className="contact_phone"> +91 22 6899 6899</NavLink>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       )}
     </div>
+
   );
 }
 
